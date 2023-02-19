5f5147fac2c3f858e572649697ea6440ad769bee
Guilherme <guilhermepolicarpio@gmail.com>
2023-02-19 19:08:31 -0300

    feat: added useAuthentication hook

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -3,38 +3,68 @@ import { styles } from "./Register.module.css"
 import { useState, useEffect } from "react";
 
 const Register = () => {
+    const[displayName, setDisplayName] = useState("");
+    const[email, setEmail] = useState("");
+    const[password, setPassword] = useState("");
+    const[confirmPassword, setConfirmPassword] = useState("");
+    const[error, setError] = useState("");
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+
+        setError("")
+
+        const user = [displayName, email, password]
+        
+        if(password !== confirmPassword){
+            setError("The passwords doesn't match");
+            return
+        }
+   
+    }
+    
     return (
         <div>
             <h1>Register to post</h1>
             <p>Create your user and share your stories</p>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <span>Name </span>
                     <input
                         type="text"
                         name="displayName"
                         required
-                        placeholder="Username" />
+                        placeholder="Username"
+                        value={displayName}
+                        onChange={(e)=> setDisplayName(e.target.value)} />
                     <span>E-mail </span>
                     <input
                         type="email"
                         name="email"
                         required
-                        placeholder="E-mail" />
+                        placeholder="E-mail" 
+                        value={email}
+                        onChange={(e)=> setEmail(e.target.value)} />
+                    
                     <span>Password </span>
                     <input
                         type="password"
                         name="password"
                         required
-                        placeholder="Password" />
+                        placeholder="Password" 
+                        value={password}
+                        onChange={(e)=> setPassword(e.target.value)}/>
                     <span>Confirm Password </span>
                     <input
                         type="password"
                         name="password"
                         required
-                        placeholder="Confirm Password" />
+                        placeholder="Confirm Password"
+                        value={confirmPassword}
+                        onChange={(e)=> setConfirmPassword(e.target.value)} />
                 </label>
                 <button className="btn">Register</button>
+                {error && <p className="error">{error}</p>}
             </form>
         </div>
     )
