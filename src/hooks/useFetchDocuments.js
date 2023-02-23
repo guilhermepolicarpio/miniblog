c01254df5728001909a44675adccd0b49b261a35
Guilherme <guilhermepolicarpio@gmail.com>
2023-02-23 12:17:45 -0300

    feat: show posts on feed

---
+++ src/hooks/useFetchDocuments.js
@@ -0,0 +1,56 @@
+import { db } from "../firebase/config";
+import { useState, useEffect } from 'react'
+import {
+    collection,
+    query,
+    orderBy,
+    onSnapshot,
+    where,
+
+} from "firebase/firestore";
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+
+    const [documents, setDocuments] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(null);
+    const [cancelled, setCancelled] = useState(false);
+
+    useEffect(() => {
+        async function loadData() {
+            if (cancelled) {
+                return;
+            }
+
+            setLoading(true);
+
+            const collectionRef = await collection(db, docCollection);
+
+            try {
+                let q;
+
+                q = await query(collectionRef, orderBy("createdAt", "desc"));
+                await onSnapshot(q, (querySnapsot) => {
+                    setDocuments(
+                        querySnapsot.docs.map((doc) => ({
+                            id: doc.id,
+                            ...doc.data(),
+                        }))
+                    )
+                });
+            } catch (error) {
+                console.log(error);
+                setError(error.message);
+            }
+                setLoading(false)
+            }
+            loadData();
+
+        }, [docCollection, search, uid, cancelled]);
+
+    useEffect(() => {
+        return () => setCancelled(true);
+    }, []);
+
+    return { documents, loading, error };
+};
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,10 +3,12 @@ import styles from "./Home.module.css"
 
 import { useNavigate, Link } from "react-router-dom"
 import { useState } from "react"
+import { useFetchDocuments } from "../../hooks/useFetchDocuments"
+import PostDetail from "../../components/PostDetail"
 
 const Home = () => {
     const [query, setQuery] = useState("");
-    const [posts] = useState([]);
+    const {documents: posts, loading} = useFetchDocuments("posts");
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -26,7 +28,10 @@ const Home = () => {
                 <button className="btn btn-dark">Search</button>
             </form>
             <div>
-            <h1>Posts...</h1>
+                {loading && <p>Loading...</p>}
+            {posts && posts.map((post) =>
+                <PostDetail key = {post.id} post={post}/>
+            )}
             {posts && posts.length === 0 &&(
                 <div className={styles.noposts}>
                     <p>No posts found</p>
